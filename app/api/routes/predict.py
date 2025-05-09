72814d2ed44a2da946108c80965e65983db8762c
AdeelFeroz <adil.feroz.7@gmail.com>
2025-05-10 01:50:42 +0500

    updated architecture

---
+++ app/api/routes/predict.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter, HTTPException, Header
+
+router = APIRouter()
---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from app.api.routes.predict import router
+
+app = FastAPI(title='ZypherAI')
+
+app.include_router(router)
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run (
+        "app.main:app",
+        host="0.0.0.0",
+        port=8080,
+        reload=True
+    )
